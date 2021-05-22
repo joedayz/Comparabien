42cb63c826e13656f067dd415f1bac7bb310d7d2
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-05-22 03:05:26 -0500

    deposito plazo first draft

---
+++ backend/ConcentradorBackend/Interfaces/IConsultaProductoService.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConcentradorBackend.Dtos.Request;
+using ConcentradorBackend.Models;
+
+namespace ConcentradorBackend.Interfaces
+{
+    public interface IConsultaProductoService
+    {
+        List<ConsultaEntidadProducto> consulta(ConsultaProductoFinancieroRequest request, int pagina);
+    }
+}
---
+++ backend/ConcentradorBackend/Models/ConcentradorDBContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcentradorBackend.Models
+{
+    public partial class ConcentradorDBContext: DbContext
+    {
+        public ConcentradorDBContext()
+        {
+        }
+
+        public ConcentradorDBContext(DbContextOptions<ConcentradorDBContext> options)
+            : base(options)
+        {
+        }
+        
+        public virtual DbSet<Opcion> Opcion { get; set; }
+        public virtual DbSet<Departamento> Departamento { get; set; }
+        public virtual DbSet<ConsultaEntidadProducto> ConsultaEntidadProducto { get; set; }
+       
+    }
+}
